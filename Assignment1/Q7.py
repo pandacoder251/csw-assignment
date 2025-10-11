8532710ddb8e87faeaae4053bac677b1f12ca168
Pousali <pousalidolai59@gmail.com>
2025-10-11 09:12:24 +0000

    updated

--- Assignment1/Q7.py
+++ Assignment1/Q7.py
@@ -1,64 +1,10 @@
-def validate_password(pass):
-    valid = True
-    special_chars = "!@#$%"
 
-    #Rule1
-    if len(password)<8:
-        print("The password must contain atleast 8 characters")
-        valid = False
+sentence = input("Enter a sentence: ")
+separator = input("Enter a custom separator: ")
 
-    #Rule2
-    has_upper = False
-    for ch in password:
-        if 'A' <= ch <= 'Z':
-            has_upper = 'True'
-            break
+cleaned = ''.join(ch for ch in sentence if ch not in string.punctuation)
+words = cleaned.lower().split()
+words.sort(reverse=True)
 
-        if not has_upper :
-            print("The password must include atleast one lowercase letter(a-z)")
-            valid = 'False'
-
-    #Rule 3   
-
-    has_lower = False
-    for ch in password:
-        if 'A' <= ch <= 'Z':
-            has_upper = 'True'
-            break
-
-        if not has_lower :
-            print("The password must include atleast one lowercase letter(A-Z)")
-            valid = 'False'
-
-    #Rule 5
-
-    has_digit = False
-    for ch in password:
-        if '0' <= ch <= '9':
-            has_digit = 'True'
-            break
-
-        if not has_digit :
-            print("The password must contain one special character from the set !@#$%")
-            valid = 'False' 
-
-
-     #Rule 6
-
-    has_space = False
-    for ch in password:
-        if ch== " " or ch== "\t" or ch== "\n":
-            has_space = 'True'
-            break
-
-        if not has_space :
-            print("The password should not have whitespace.")
-            valid = 'False'   
-
-
-user_pasword = input("Enter password: ")
-if validate_password(user_password):
-    print("password is valid")
-
-else:
-    pin("Password is invalid")    
+result = separator.join(words)
+print("Output:", result)
