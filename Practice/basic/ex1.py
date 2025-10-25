3fdf64a76b7e32ddb62543d1b48a35176a3976e6
Pousali <pousalidolai59@gmail.com>
2025-10-25 15:27:11 +0000

    update

--- Practice/basic/ex1.py
+++ Practice/basic/ex1.py
@@ -4,3 +4,4 @@ m3 = 70
 avg =(m1+m2+m3)//3
 print ( "mark 1:" , m1 , " mark2: "  , m2,  " mark 3: " , m3)
 print("Average Mark : " , avg) 
+
---
+++ Practice/loop statement/ex.py
@@ -0,0 +1,44 @@
+'''a = 3
+b = 4
+print("a = " ,a )
+print(" b = " ,b)
+a = a+b
+b = a- b
+a = a-b
+print("a = " ,a )
+print(" b = " ,b)'''
+
+'''a = float(input("enter a number: "))
+b = float (input("enter a number:"))
+oper = input("enter the operation + , - , *, ** , /,//,% : ")
+
+if oper == '+':
+    print("a+b = ", a+ b)
+elif oper == '-':
+    print("a-b = " ,a+ b)
+elif oper == '*':
+    print("a*b = ", a* b)
+elif oper == '**':
+    print("a^b = ", a**b)
+elif oper == '/':
+    if b == 0 :
+        print("Division by Zero")
+    print("a/b = ", a/b)     
+elif oper == '//':
+    if b == 0 :
+        print("Division by Zero")
+    print("a//b = ", a//b)   
+elif oper == '%':
+    if b == 0 :
+        print("Division by Zero")
+    print("a%b = ", a%b)          
+else : 
+    print("Invalid Operator!")            '''
+
+a = int(input("enter 1st num: "))
+b = int(input("enter 2nd num: "))
+c = int(input("enter 3rd num: "))
+if a < b and b < c:
+    print(" c is largest")
+elif a> b and b > c:
+    print()
