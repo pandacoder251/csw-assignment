4eee983867de27005ee2b4fade84c8b7e291d28d
Pousali <pousalidolai59@gmail.com>
2025-11-05 06:58:03 +0000

    practised

---
+++ Practice/string/Q.py
@@ -0,0 +1,31 @@
+#1
+def compare_no_case(str1, str2):
+    return str1.lower() == str2.lower()
+print(compare_no_case("cat", "CAT"))  # True
+
+#2
+first = 'will'
+second = 'arm'
+print(first>second)  # False
+print(first<second)  # True
+print(first==second) # False
+print(first!=second) # True
+print(first>=second) # False
+print(first<=second) # True
+
+#3
+s = 'Python is Fun'
+s1 =s[2:6]
+s2 = s[-4:-1]
+s3 = s[1:-1]
+s4 = s[::3]
+s5 = s[5:9:2]
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
+
+
+#4
+
